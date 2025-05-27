e4324002675656fa502240e6c1be1186c913d403
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-05-27 02:54:30 +0200

    texts

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dèmos : The people</title>
+    <title>Dèmos : The People</title>
     <meta charset="UTF-8">
 </head>
 <body>
     <table style="border-collapse: collapse; width: 100%;">
         <tr>
-            <td style="width: 25%; text-align: center; padding: 10px;"><a href="index.php">Home</a></td>
+            <td style="width: 25%; text-align: center; padding: 10px;"><a href="index.php">The People</a></td>
             <?php if (is_logged_in()): ?>
-                <td style="width: 25%; text-align: center; padding: 10px;"><a href="post.php">New Post</a></td>
-                <td style="width: 25%; text-align: center; padding: 10px;"><a href="user.php?username=<?php echo htmlspecialchars($_SESSION['username']); ?>">My Profile</a></td>
-                <td style="width: 25%; text-align: center; padding: 10px;"><a href="logout.php">Logout</a></td>
+                <td style="width: 25%; text-align: center; padding: 10px;"><a href="post.php">Talk</a></td>
+                <td style="width: 25%; text-align: center; padding: 10px;"><a href="user.php?username=<?php echo htmlspecialchars($_SESSION['username']); ?>"><?php echo htmlspecialchars($_SESSION['username']); ?></a></td>
+                <td style="width: 25%; text-align: center; padding: 10px;"><a href="logout.php">Disconnect</a></td>
             <?php else: ?>
-                <td style="width: 50%; text-align: center; padding: 10px;" colspan="2"><a href="login.php">Login</a></td>
-                <td style="width: 50%; text-align: center; padding: 10px;" colspan="2"><a href="register.php">Register</a></td>
+                <td style="width: 50%; text-align: center; padding: 10px;" colspan="2"><a href="login.php">Connect</a></td>
+                <td style="width: 50%; text-align: center; padding: 10px;" colspan="2"><a href="register.php">Join</a></td>
             <?php endif; ?>
         </tr>
     </table>
