64a00c7ab2dd06464bb545ae4583f5d73ba3ea76
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-05-27 04:11:57 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <br>
-<hr>
+<hr style="color: black; opacity: 1;">
 <footer style="text-align: center;">
     <form action="bios.php" method="get" style="display: inline; padding: 10px;">
         <button type="submit">Bios</button>
